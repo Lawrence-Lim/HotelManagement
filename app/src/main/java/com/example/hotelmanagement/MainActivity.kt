03af9029a8f6a332fe23b64698a736294228a745
Lim Wei Xuan <limwx86@gmail.com>
2021-04-16 19:42:33 +0800

    Created Splash Screen and Login Page

--- app/src/main/java/com/example/hotelmanagement/MainActivity.kt
+++ app/src/main/java/com/example/hotelmanagement/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.hotelmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.content.Intent
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent( this@MainActivity, Login_Page::class.java)
+            startActivity(intent)
+            finish()
+        },4000)
     }
+
+
 }
